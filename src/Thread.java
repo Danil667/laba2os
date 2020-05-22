118c440215527b69af3483354c2b5b4af30e56d1
danil732000@gmail.com <danil732000@gmail.com>
2020-05-22 15:32:12 +0400

    Initial commit

---
+++ src/Thread.java
@@ -0,0 +1,37 @@
+public class Thread {
+    private String description = "Поток ";
+    private int maxTime;
+    private int currentTime = 0;
+
+    public Thread(String description, int maxTime) {
+        this.description += description;
+        this.maxTime = maxTime;
+    }
+
+    public int getCurrentTime() {
+        return currentTime;
+    }
+
+
+    public int getMaxTime() {
+        return maxTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void makeThread() {
+        currentTime++;
+        System.out.println(getInfo());
+    }
+
+    public boolean needTime() {
+        return maxTime > currentTime;
+    }
+
+    public String getInfo() {
+        return description + " maxTime:"
+                + maxTime + " currentTime:" + currentTime;
+    }
+}
